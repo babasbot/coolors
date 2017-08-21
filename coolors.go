fa2895c1f859e649dba4d43ec5d7bba166427b57
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-08-21 08:24:29 -0500

    Creates the Green(g uint32) color.RGBA function

--- coolors.go
+++ coolors.go
@@ -5,3 +5,7 @@ import "image/color"
 func Red(r uint32) color.RGBA {
 	return color.RGBA{uint8(r >> 8), 0, 0, 255}
 }
+
+func Green(g uint32) color.RGBA {
+	return color.RGBA{0, uint8(g), 0, 255}
+}
