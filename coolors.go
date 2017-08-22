8f3dcedf568d4f31fd96cf154cd4db191626bc26
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-08-21 22:47:18 -0500

    Creates the Color(r, g, b uint32, col color.RGBA) color.RGBA function

--- coolors.go
+++ coolors.go
@@ -13,3 +13,14 @@ func Green(g uint32) color.RGBA {
 func Blue(b uint32) color.RGBA {
 	return color.RGBA{0, 0, uint8(b >> 8), 255}
 }
+
+func Color(r, g, b uint32, col color.RGBA) color.RGBA {
+	pixel := color.RGBA{}
+
+	pixel.R = uint8(r>>8) & col.R
+	pixel.G = uint8(g>>8) & col.G
+	pixel.B = uint8(b>>8) & col.B
+	pixel.A = 255
+
+	return pixel
+}
